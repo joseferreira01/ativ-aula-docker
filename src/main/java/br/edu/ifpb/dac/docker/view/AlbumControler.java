019503736e4efb1951cba7200196dd620b8c167e
Jose <joseifpb2015@gmail.com>
2018-05-17 10:21:28 -0300

    add servico controler

--- src/main/java/br/edu/ifpb/dac/docker/view/AlbumControler.java
+++ src/main/java/br/edu/ifpb/dac/docker/view/AlbumControler.java
@@ -5,6 +5,7 @@
  */
 package br.edu.ifpb.dac.docker.view;
 
+import br.edu.ifpb.dac.docker.entidades.Album;
 import br.edu.ifpb.dac.docker.repositorio.servico.AlbumServico;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -17,11 +18,15 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class AlbumControler {
+    private Album album;
     private AlbumServico servico;
     @PostConstruct
     public void init(){
         this.servico = new AlbumServico();
+        this.album = new Album();
     }
-    public void salvar
+    public void salvar(){
+        
+    }
     
 }
